61e604ae4cc208fd977210eb2a3e224ac942bd91
Yimmy-PLL <202311474@urp.edu.pe>
2025-11-19 21:11:47 -0500

    Subiendo proyecto UV completo

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 import pandas as pd
+import os
 
 from modelo_uv import predecir_uv
 
@@ -53,4 +54,5 @@ def prediccion_uv():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
